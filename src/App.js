232bbb72229c6e9ea1de546637f22df34067249f
Sunny Raj <sunny.raj@happiestminds.com>
2024-07-30 20:36:58 +0530

    theme setup

--- src/App.js
+++ src/App.js
@@ -1,23 +1,24 @@
-import logo from './logo.svg';
-import './App.css';
+// Import statements for your components
+import Sidebar from "./Components/Sidebare"; // Make sure the filename is correct
+import Footer from "./Components/Footer";
+import Dashboard from "./Components/Dashboard";
+
+// Import BrowserRouter from react-router-dom
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import ControlSidebar from "./Components/ControlSidebar";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div class="wrapper">
+      
+    <Router>
+      <Sidebar />
+      <Routes>
+        <Route path="/" element={<Dashboard />} />
+      </Routes>
+      <ControlSidebar/>
+      <Footer />
+    </Router>
     </div>
   );
 }
