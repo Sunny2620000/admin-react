175e66234a597d47d470f01b7997c6322b567ed0
Sunny Raj <sunny.raj@happiestminds.com>
2024-07-31 16:00:54 +0530

    i have change href to to in sidebare component

--- src/Components/Sidebare.js
+++ src/Components/Sidebare.js
@@ -5,10 +5,10 @@ export default function Sidebar() {
     return (
         <aside className="main-sidebar sidebar-dark-primary elevation-4">
     {/* <!-- Brand Logo --> */}
-    <a href="index3.html" className="brand-link">
+    <Link to="/" className="brand-link">
       <img src="assets/dist/img/AdminLTELogo.png" alt="AdminLTE Logo" className="brand-image img-circle elevation-3" style={{"opacity": ".8"}}/>
       <span className="brand-text font-weight-light">AdminLTE 3</span>
-    </a>
+    </Link>
 
     {/* <!-- Sidebar --> */}
     <div className="sidebar">
